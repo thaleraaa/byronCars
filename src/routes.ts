c7fff13afb7f346bd99afd1635150ab9540451b9
Thales <d2024001956@unifei.edu.br>
2026-02-28 16:54:49 -0300

    feat: Login e autentincação com token feito

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router, Request, Response } from "express";
 import multer from "multer";
 import uploadConfig from "./config/multer"
 import { CreateUserController } from "./controllers/User/CreateUserController";
+import { AuthUserController } from "./controllers/User/AuthUserController";
 
 const router = Router();
 const upload = multer(uploadConfig.upload("./tmp"));
@@ -12,5 +13,6 @@ router.get("/test",(request: Request, response: Response) => {
 
 // User Routes
 router.post("/user", new CreateUserController().handle);
+router.post("/session", new AuthUserController().handle);
 
 export { router };
---
+++ src/services/AuthUserService.ts
@@ -0,0 +1,53 @@
+import prismaClient from "../prisma";
+import { AuthUserRequest } from "../models/interfaces/User/Auth/AuthUserRequest";
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+
+class AuthUserService {
+    async execute ({email, password}:AuthUserRequest) {
+        if (!email) {
+            throw new Error ("Email is required");
+        }
+        if (!password) {
+            throw new Error ("Password is required");
+        }
+        
+        const user = await prismaClient.user.findFirst({
+            where: {
+                email: email
+            }
+        });
+
+        if(!user) {
+            throw new Error ("Email not exists!");
+        }
+
+        const passwordValided = compare(password, user?.password);
+
+        if (!passwordValided) {
+            throw new Error ("Wrong password!");
+        }
+
+        const token = sign(
+            {
+                name: user?.name,
+                email: user?.email
+            },
+            process.env.JWT_SECRET as string,
+            {
+                subject: user?.id,
+                expiresIn: "30d"
+            }
+        )
+
+        return {
+            id: user?.id,
+            name: user?.name,
+            email: user?.email,
+            token: token
+        }
+
+    }
+}
+
+export { AuthUserService }
